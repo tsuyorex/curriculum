2e9db5e5052af317fec20008f0ef07974bdf51d2
tsuyorex <98862849+tsuyorex@users.noreply.github.com>
2022-02-13 20:22:46 +0900

    課題2-1の提出
    課題２－１を提出します。

---
+++ Javastudy/chapter1/src/chapter1/Ex1.java
@@ -0,0 +1,13 @@
+package chapter1;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("hello java");
+		System.out.print("test");
+//		出力です
+
+	}
+
+}
