8e731924d4791b061640f5eac1b302751074bcc4
tsuyorex <98862849+tsuyorex@users.noreply.github.com>
2022-02-18 21:24:29 +0900

    課題2-3の修正
    課題2-3を修正しました。

--- Javastudy/2-3/src/study/Task.java
+++ Javastudy/2-3/src/study/Task.java
@@ -6,14 +6,18 @@ package study;
     /**
      * タスクの実行
      */
+//		オーバーライドのテストメソッド
+		public int plus(int a, int b, int c) {
+			return a*b*c;
+		}
     public void doTask() {
 
         // ② Calculator.javaのすべてのオーバーロードメソッド「plus」に適当な引数を与え、下記画像のよう出力されるようコーディングしなさい。
         // 尚、「どのクラスから呼び出しているか」を明確にするため、plus()には呼び出し元のキーワードを付与すること。
 
-    	System.out.println("plusメソッドの因数が一つの場合："+this.plus(10));
-    	System.out.println("plusメソッドの因数が二つの場合："+this.plus(10,20));
-    	System.out.println("plusメソッドの因数が三つの場合："+this.plus(10,20,30));
+    	System.out.println("plusメソッドの因数が一つの場合："+super.plus(10));
+    	System.out.println("plusメソッドの因数が二つの場合："+super.plus(10,20));
+    	System.out.println("plusメソッドの因数が三つの場合："+super.plus(10,20,30));
     	
     }
 }
